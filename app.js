ccf62114736cbc9497290a81339ace2e7d1255fd
Ali Whiteford <144415117+aliotako@users.noreply.github.com>
2023-11-28 19:23:10 +1100

    Update app.js
    removing client secret key

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const app = express();
 const PORT = 3000;
 
 // Spotify API credentials
-const clientId = '9aa83863e60546c4bb2a5727ea722094';
-const clientSecret = 'e3ea44db904c49ec8d42cfdcfab0f735';
-const rapidApiKey = 'de92c1c9f7mshfa6eecf68db49f0p15aae5jsnecf827d8f83f';
+';
 
 // Spotify23 API endpoint
 const spotifyApiUrl = 'https://spotify23.p.rapidapi.com';
 
 // Middleware to set headers for API requests
-app.use((req, res, next) => {
-  res.header('X-RapidAPI-Host', 'spotify23.p.rapidapi.com');
-  res.header('X-RapidAPI-Key', rapidApiKey);
+
   next();
 });
 
