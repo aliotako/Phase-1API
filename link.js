dd2c6282b930ed690d7623bdb4ba6630bb35df8d
Ali Whiteford <aliwhiteford@bigpond.com>
2023-11-30 17:33:10 +1100

    html css js. added preloading button

--- link.js
+++ link.js
@@ -1,13 +1,13 @@
 let MixString = ["ec12fa0", "17790", "6e4fd6", "72cd83", "defab"];
-let secretOne  = "76";
-let secretTwo = "2a490";
-let secret = secretTwo.split("").reverse().join("");
+let numOne  = "76";
+let numTwo = "2a490";
+let num = numTwo.split("").reverse().join("");
 
 stepOne = MixString.join("").split("").reverse().join("");
-stepTwo = stepOne.split(secretOne);                 
+stepTwo = stepOne.split(numOne);                 
 stepThree = stepTwo[0] + stepTwo[1];
 
-key = stepThree + secret;
+key = stepThree + num;
 
 
 
--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-let printKey = document.getElementById("printKey");
-printKey.innerHTML = `KEY: ${key}`
+
+const loaderEl = document.getElementById("js-preloader");
+const gameList = document.querySelector(".gameList");
+const loadMoreGamesBtn = document.querySelector(".main-button")
+let nextGameListUrl = null;
+
+// conditions for updated values (date)
+const url = `https://api.rawg.io/api/games?key=${key}&dates=2023-01-01,2023-11-301&ordering=-added`;
+
+// console.log(url, "check")
+
+function loadGames(url) {
+    loaderEl.classList.remove("loaded");
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
+loadGames(url);
+
+
+// const getPlatformStr = (platforms) => {
+//     const platformStr = platforms.map(pl => pl.platform.name).join(",")
+// }
